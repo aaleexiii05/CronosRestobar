3f70dbd646a28cac6d10434c4c08171796ebb04c
aaleexiii05 <aaleexiibot05@gmail.com>
2026-04-19 18:05:37 -0500

    Configuracion inicial: Pom y estructura

--- demo/src/test/java/com/example/cronos/CronosApplicationTests.java
+++ demo/src/test/java/com/example/cronos/CronosApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.cronos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CronosApplicationTests {
 
 	@Test
 	void contextLoads() {
